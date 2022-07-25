424bb200e019558073cd0009411be82ab25876b0
Miouss <samir.ghabi@gmail.com>
2022-07-25 23:21:17 +0200

    added fontaw library to make icons usable globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
 import Desktop from "./components/Desktop";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faWindows } from "@fortawesome/free-brands-svg-icons";
+
+import {
+  faTerminal,
+  faXmark,
+  faWindowMinimize,
+  faMaximize,
+  faCode
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
 function App() {
-  return (
-    <Desktop />
+  library.add(
+    faWindows,
+    faTerminal,
+    faXmark,
+    faWindowMinimize,
+    faMaximize,
+    faCode
   );
+
+  return <Desktop />;
 }
 
 export default App;
