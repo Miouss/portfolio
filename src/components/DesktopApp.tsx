f8ed40dc9483ee3ecf24ca19b0c4af20963a7f16
Miouss <samir.ghabi@gmail.com>
2022-07-27 20:25:49 +0200

    Changed to handle multiples launchable windows

--- src/components/DesktopApp.tsx
+++ src/components/DesktopApp.tsx
@@ -1,25 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ReactComponentElement, ReactElement } from "react";
+import { ReactElement } from "react";
 
 import "../styles/DesktopApp.css";
 
 interface Props {
   iconName: any;
   appName: string;
-  setDisplayApp: (param: null | ReactElement) => void;
-  component: ReactElement
+  displayApp: Array<ReactElement> | null;
+  setDisplayApp: (param: Array<ReactElement>) => void;
+  component: ReactElement;
 }
 
-function DesktopApp({ iconName, appName, setDisplayApp, component }: Props) {
+function DesktopApp({
+  iconName,
+  appName,
+  displayApp,
+  setDisplayApp,
+  component,
+}: Props) {
   const iconClass: string = `desktop-app-${iconName}`;
 
+  function runApp() {
+    if (displayApp === null) {
+      setDisplayApp([component]);
+    } else {
+      let isAppRunning = false;
+
+      displayApp.forEach((componentStored) => {
+        if (componentStored.key === component.key) {
+          alert("App is already running");
+          isAppRunning = true;
+        }
+      });
+
+      if(!isAppRunning){
+        setDisplayApp([...displayApp, component]);
+      }
+    }
+  }
+
   return (
-      <div className={iconClass} onClick={() => setDisplayApp(component)}>
-        <span>
-          <FontAwesomeIcon icon={iconName} color="white" />
-        </span>
-        <div>{appName}</div>
-      </div>
+    <div className={iconClass} onClick={runApp}>
+      <span>
+        <FontAwesomeIcon icon={iconName} color="white" />
+      </span>
+      <div>{appName}</div>
+    </div>
   );
 }
 
