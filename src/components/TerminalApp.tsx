5cd8b0d5c86c47429b20e7af979d12217a270bdc
Miouss <samir.ghabi@gmail.com>
2022-07-27 20:24:45 +0200

    Changed to handle closable windows independently

--- src/components/TerminalApp.tsx
+++ src/components/TerminalApp.tsx
@@ -4,15 +4,17 @@ import WindowApp from "./WindowApp";
 import "../styles/TerminalApp.css";
 
 type Props = {
-  setCloseApp: (param: boolean) => void;
+  setCloseApp: (param: string | null) => void;
+  componentKey: string;
 };
 
-function TerminalApp({ setCloseApp }: Props) {
+function TerminalApp({ setCloseApp, componentKey }: Props) {
   return (
     <WindowApp
       windowTitle="Terminal"
       setCloseApp={setCloseApp}
       contentComponent={<InteractiveTextArea />}
+      componentKey={componentKey}
     />
   );
 }
