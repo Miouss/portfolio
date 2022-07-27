63bedf4d2b1a3b43fd9ee1f17fd0156193c183ad
Miouss <samir.ghabi@gmail.com>
2022-07-27 20:26:32 +0200

    Changed to handle windows close/open behaviors

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import DesktopTaskBar from "./DesktopTaskBar";
 import DesktopApp from "./DesktopApp";
 import TerminalApp from "./TerminalApp";
@@ -5,46 +6,55 @@ import TerminalApp from "./TerminalApp";
 import "../styles/Desktop.css";
 import { ReactElement, useEffect, useState } from "react";
 
-
 function Desktop() {
-  let [displayApp, setDisplayApp] = useState<ReactElement | null>(null);
-  let [closeApp, setCloseApp] = useState<boolean>(false);
+  let [displayApp, setDisplayApp] = useState<Array<ReactElement>>([]);
+  let [closeApp, setCloseApp] = useState<string | null>(null);
 
-  let appsList : Array<ReactElement> = [];
+  let appsList: Array<ReactElement> = [];
 
-  function addApp(icon: string, app: string, component: ReactElement ){
-    appsList.push(<DesktopApp
-      iconName={icon}
-      appName={app}
-      setDisplayApp={setDisplayApp}
-      component={component}
-    />);
+  function addApp(icon: string, app: string, component: ReactElement) {
+    appsList.push(
+      <DesktopApp
+        key={`app${appsList.length}`}
+        iconName={icon}
+        appName={app}
+        displayApp={displayApp}
+        setDisplayApp={setDisplayApp}
+        component={component}
+      />
+    );
   }
 
-  addApp("terminal", "Terminal", <TerminalApp setCloseApp={setCloseApp} />);
-
-  useEffect(
-    function resetCloseButton() {
-      setCloseApp(false);
-    },
-    [displayApp]
+  addApp(
+    "terminal",
+    "Terminal",
+    <TerminalApp
+      key="terminal"
+      setCloseApp={setCloseApp}
+      componentKey="terminal"
+    />
   );
 
-  useEffect(
-    function resetDisplayApp() {
-      setDisplayApp(null);
-    },
-    [closeApp]
-  );
+  useEffect(() => {
+    setCloseApp(null);
+  }, [displayApp]);
+
+  useEffect(() => {
+    let appsCurrentlyRunning: Array<ReactElement> = displayApp.filter(
+      (component: ReactElement) => {
+        return component.key !== closeApp;
+      }
+    );
+
+    setDisplayApp(appsCurrentlyRunning);
+  }, [closeApp]);
 
   return (
     <div id="desktop">
       <div style={{ position: "absolute", top: "100px", left: "100px" }}>
         {displayApp}
       </div>
-      <div id="desktop-app-section">
-        {appsList}
-      </div>
+      <div id="desktop-app-section">{appsList}</div>
       <DesktopTaskBar />
     </div>
   );
