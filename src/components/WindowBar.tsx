81cbdaa5f9d1fd08ceafb01fc705eeac48057f81
Miouss <samir.ghabi@gmail.com>
2022-07-30 15:30:22 +0200

    Use of Material UI

--- src/components/WindowBar.tsx
+++ src/components/WindowBar.tsx
@@ -1,52 +1,64 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import Typography from "@mui/material/Typography";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
 
-import CloseIcon from '@mui/icons-material/Close';
-import CropFreeIcon from '@mui/icons-material/CropFree';
-import MinimizeIcon from '@mui/icons-material/Minimize';
+import CloseIcon from "@mui/icons-material/Close";
+import CropFreeIcon from "@mui/icons-material/CropFree";
+import MinimizeIcon from "@mui/icons-material/Minimize";
 
 import "../styles/WindowBar.css";
 
 import { ReactElement, useState } from "react";
 
 interface Props {
-  windowTitle: string,
-  setCloseApp: (param: string | null) => void,
-  componentKey: string,
-  handleMousePressed: (event: React.MouseEvent<HTMLDivElement>) => void,
-  appIcon : ReactElement
+  windowTitle: string;
+  setCloseApp: (param: string | null) => void;
+  componentKey: string;
+  handleMousePressed: (event: React.MouseEvent<HTMLDivElement>) => void;
+  appIcon: ReactElement;
 }
 
-function WindowBar({ windowTitle, setCloseApp, componentKey, handleMousePressed, appIcon }: Props) {
+function WindowBar({
+  windowTitle,
+  setCloseApp,
+  componentKey,
+  handleMousePressed,
+  appIcon,
+}: Props) {
   let [closeButtonColor, changeCloseButtonColor] = useState<string>("black");
+  let [bgColor, changeBgColor] = useState<string>("initial");
 
   return (
     <div className="window-bar">
-      <div className="window-bar-app-icon">
-        {appIcon}
-      </div>
-      <div
-        className="window-bar-title"
+      {appIcon}
+      <Typography 
+        style={{flexGrow : 1}}
         onMouseDown={handleMousePressed}
         onMouseUp={handleMousePressed}
       >
         {windowTitle}
-      </div>
-      <div className="window-bar-functionnal-icons">
-        <div className="minimize-button">
+      </Typography>
+
+      <ButtonGroup variant="outlined" color="inherit">
+        <Button>
           <MinimizeIcon />
-        </div>
-        <div className="resize-button">
+        </Button>
+        <Button>
           <CropFreeIcon />
-        </div>
-        <div
-          className="close-button"
-          onMouseOver={() => changeCloseButtonColor("white")}
-          onMouseLeave={() => changeCloseButtonColor("black")}
+        </Button >
+        <Button style={{ backgroundColor: bgColor}}
           onClick={() => setCloseApp(componentKey)}
+          onMouseOver={() => {
+            changeBgColor("red"); changeCloseButtonColor("white")
+          }}
+          onMouseLeave={() => {
+            changeBgColor("initial"); changeCloseButtonColor("initial") 
+          }}
         >
-          <CloseIcon sx={{color: closeButtonColor}} />
-        </div>
-      </div>
+          <CloseIcon style={{ color: closeButtonColor }} />
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
