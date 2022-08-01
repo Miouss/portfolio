8f48f6441150e7cebef3af622f07b67e7248dc03
Miouss <samir.ghabi@gmail.com>
2022-08-01 13:07:36 +0200

    windows' icon switchable

--- src/components/AppList.tsx
+++ src/components/AppList.tsx
@@ -23,22 +23,24 @@ appList["PDFViewer"] = {
   icon: <ContactMailIcon />,
 };
 
-export const getApp = (name: String) => {
+export const getApp = (name: string) => {
   return appList[`${name}`].app;
 };
 
-export const getIcon = (name: String) => {
+export const getIcon = (name: string) => {
   return appList[`${name}`].icon;
 };
 
-export const windowsIcon = (
-  <svg
-    fill="white"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 30 30"
-    width="30px"
-    height="30px"
-  >
-    <path d="M12 16L3 16 3 23.75 12 24.988zM12 5L3 6.25 3 14 12 14zM14 4.75L14 14 27 14 27 3zM14 16L14 25.25 27 27 27 16z" />
-  </svg>
-);
+export const windowsIcon = (color: string) => {
+  return (
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 30 30"
+      width="30px"
+      height="30px"
+    >
+      <path d="M12 16L3 16 3 23.75 12 24.988zM12 5L3 6.25 3 14 12 14zM14 4.75L14 14 27 14 27 3zM14 16L14 25.25 27 27 27 16z" />
+    </svg>
+  );
+};
