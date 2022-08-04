3e6c51f8ac7babaac5e6fd81e7e0d717b6f9719f
Miouss <samir.ghabi@gmail.com>
2022-08-05 00:52:28 +0200

    WindowBar with move function isolated to a comp

---
+++ src/components/custom/MovableBar.tsx
@@ -0,0 +1,61 @@
+import { useRef, useState, useEffect } from "react";
+
+import "../../styles/WindowBar.css";
+
+interface Offset{
+  left: string,
+  top: string
+}
+
+export function MovableBar({className, children}) {
+  const windowBarRef = useRef<HTMLDivElement>(null) as any;
+  const [mouseIsPressed, setMouseIsPressed] = useState<boolean>(false);
+  const [originalOffset, setOriginalOffset] = useState<Offset>();
+
+  const handleMouseMovement = (event): void => {
+    event.preventDefault();
+    
+    if(mouseIsPressed) {
+        windowBarRef.current!.offsetParent!.style.left =
+        windowBarRef.current!.offsetParent!.offsetLeft   + event.movementX + "px";
+        windowBarRef.current!.offsetParent!.style.top =
+        windowBarRef.current!.offsetParent!.offsetTop + event.movementY + "px";
+    }
+  };
+
+  useEffect(() => {
+    if (mouseIsPressed) {
+      document.onmouseup = () => setMouseIsPressed(false);
+
+      const windowBarRefCurrent = windowBarRef.current!;
+
+      return () => {
+        const windowPos =
+        windowBarRefCurrent.offsetParent!.getBoundingClientRect();
+        
+        if (windowPos.top < 0 || windowPos.bottom > 1080) {
+            windowBarRefCurrent.offsetParent!.style.top = originalOffset!.top;
+            windowBarRefCurrent.offsetParent!.style.left = originalOffset!.left;
+        }
+
+        document.onmouseup = () => {
+          return false;
+        };
+      };
+    }else{
+      setOriginalOffset({left: windowBarRef.current!.offsetParent!.offsetLeft + "px", top: windowBarRef.current!.offsetParent!.offsetTop + "px"})
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mouseIsPressed]);
+
+  return (
+    <div
+    className={className}
+      ref={windowBarRef}
+      onPointerMove={(event) => handleMouseMovement(event)}
+      onPointerDown={() => setMouseIsPressed(true)}
+    >
+      {children}
+    </div>
+  );
+}
