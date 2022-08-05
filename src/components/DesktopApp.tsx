f4a9687a85b3e335d51db239f3bf7897184edb0b
Miouss <27730428+Miouss@users.noreply.github.com>
2022-08-05 19:01:15 +0200

    Desktop app behavior even closer to windows' apps (#8)
    * CSS that centering desktop app
    
    * Removed css that is now is handle by state
    
    * Added border hidden to avoid display on 1st disply
    
    * Mimic behavior of windows' app
    
    * fixed bad css name
    
    * cursor no more changing on hovering app's name

--- src/components/DesktopApp.tsx
+++ src/components/DesktopApp.tsx
@@ -1,17 +1,81 @@
 import "../styles/DesktopApp.css";
 
-import { openApp, useAppDispatch } from "../redux";
+import { openApp, RootState, useAppDispatch, focusApp } from "../redux";
 import { AppIcon } from "./AppList";
+import { useState } from "react";
+
+import { delay } from "../assets/usefulFunction";
+import { useSelector } from "react-redux";
 
 interface Props {
-  appName : string
+  appName: string;
+}
+
+interface AppStyle {
+  borderStyle?: string;
+  padding?: string;
+  background?: string;
+  cursor?: string;
 }
 
 function DesktopApp({ appName }: Props) {
+  const isRunning = useSelector((state : RootState) => state.apps[appName].isRunning);
   const dispatch = useAppDispatch();
+  
+  const [appStyle, setAppStyle] = useState<AppStyle>();
+
+  const pointerEnterStyle = {
+    borderStyle: "solid",
+    padding: "calc(1rem - 1px)",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+  };
+
+  const pointerLeaveStyle = {
+    borderStyle: "hidden",
+    padding: "1rem",
+  };
+
+  const pointerDownStyle = {
+    borderStyle: "solid",
+    padding: "calc(1rem - 1px)",
+    backgroundColor: "unset",
+  };
+
+  const handlePointerEvent = (event) => {
+    if (event.type === "pointerenter") {
+      setAppStyle(pointerEnterStyle);
+    } else if (event.type === "pointerleave") {
+      setAppStyle(pointerLeaveStyle);
+    } else {
+      setAppStyle(pointerDownStyle);
+    }
+  };
+
+  const simulateAppLoading = async() => {
+    if(isRunning){
+      dispatch(focusApp(appName));
+    }else{
+      setAppStyle({ ...appStyle, cursor: "progress" });
+      await delay(300);
+      setAppStyle((appStyle) => {
+        const newStyle = {...appStyle};
+  
+        newStyle.cursor = "default";
+        return newStyle;
+      });
+      dispatch(openApp(appName));
+    }
+  };
 
   return (
-    <div className="desktop-app-terminal" onClick={() => dispatch(openApp(appName))}>
+    <div
+      className="desktop-app"
+      style={appStyle}
+      onPointerEnter={(event) => handlePointerEvent(event)}
+      onPointerLeave={(event) => handlePointerEvent(event)}
+      onClick={(event) => handlePointerEvent(event)}
+      onDoubleClick={simulateAppLoading}
+    >
       <span>
         <AppIcon appName={appName} />
       </span>
