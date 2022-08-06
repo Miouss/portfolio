3b02f502f4c4bffde65d1b2eabecd489c35641e9
Miouss <samir.ghabi@gmail.com>
2022-08-07 01:41:40 +0200

    Changed to add context menu

--- src/components/DesktopTaskBarApp.tsx
+++ src/components/DesktopTaskBarApp.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { focusApp, minimizeApp, RootState, useAppDispatch } from "../redux";
+
 import { AppIcon } from "./AppList";
+import ContextMenu from "./DesktopTaskBarContextMenu";
 
 interface Props {
   appName: string;
@@ -10,6 +12,8 @@ interface Props {
 function DesktopTaskBarApp({ appName }: Props) {
   const app = useSelector((state: RootState) => state.apps[appName]);
   const dispatch = useAppDispatch();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const [bgColor, setBgColor] = useState<string>("");
   const [lineWidth, setLineWidth] = useState<string>("");
@@ -40,13 +44,50 @@ function DesktopTaskBarApp({ appName }: Props) {
     }
   }, [opacityBgColorBoost, app.isFocused]);
 
+  const handleDocumentContextMenu = (event) => {
+    event.preventDefault();
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    if (Boolean(anchorEl)) {
+      document.addEventListener(
+        "contextmenu",
+        (event) => handleDocumentContextMenu(event),
+        true
+      );
+      return () =>
+        document.removeEventListener("contextmenu", (event) =>
+          handleDocumentContextMenu(event)
+        );
+    }
+  }, [anchorEl]);
+
+  useEffect(() => {
+    document.dispatchEvent(new MouseEvent("contextmenu"));
+  }, []);
+
   return (
     <div
+      id="basic-button"
       onClick={handleClick}
       onMouseDown={(event) => event.preventDefault()}
       onMouseEnter={(event) => handleMouseOver(event)}
       onMouseLeave={(event) => handleMouseOver(event)}
+      onContextMenu={(event) => {
+        event.preventDefault();
+        if (anchorEl === null) {
+          event.stopPropagation();
+        }
+        setAnchorEl(event.currentTarget);
+      }}
     >
+      <ContextMenu
+        anchorEl={anchorEl}
+        setAnchorEl={setAnchorEl}
+        appName={appName}
+      />
+
       <div style={{ backgroundColor: bgColor, fontSize: "1.5rem" }}>
         <AppIcon appName={appName} />
       </div>
