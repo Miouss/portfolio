30c7e4527f1d86e351e104df227da47beb7ca9df
Miouss <samir.ghabi@gmail.com>
2023-01-21 17:18:35 +0100

    Created custom hooks from ResizableDiv

---
+++ src/styles/components/apps/hooks/useUpdateDivPosition.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import dispatchResizingEvent from "../utils/dispatchResizingEvent";
+
+import { Dispatch, SetStateAction, CSSProperties } from "react";
+
+export default function useUpdateDivPosition(
+  currentResizableDivRef: HTMLDivElement | null,
+  updateDivPosition: boolean,
+  setUpdateDivPosition: Dispatch<SetStateAction<boolean>>,
+  setDynamicStyle: Dispatch<SetStateAction<CSSProperties>>
+) {
+  useEffect(() => {
+    setUpdateDivPosition((prevState) => !prevState);
+  }, []);
+
+  useEffect(() => {
+    if(currentResizableDivRef === null) return;
+    
+    const currentDimensions = currentResizableDivRef.getBoundingClientRect();
+
+    setDynamicStyle({
+      width: currentDimensions.width + "px",
+      height: currentDimensions.height + "px",
+      top: currentDimensions.top + "px",
+      left: currentDimensions.left + "px",
+      transform: "none",
+    });
+
+    dispatchResizingEvent(currentResizableDivRef, {
+      width: currentDimensions.width,
+      height: currentDimensions.height,
+    });
+  }, [updateDivPosition]);
+}
