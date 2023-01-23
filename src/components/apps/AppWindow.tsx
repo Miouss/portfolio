f0dcdfbad9cc08cf71ee6e6b91b0b674d5bb25a0
Miouss <samir.ghabi@gmail.com>
2023-01-23 10:26:59 +0100

    removed useless import

--- src/components/apps/AppWindow.tsx
+++ src/components/apps/AppWindow.tsx
@@ -1,13 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Box } from "@mui/material";
-
 import Window from "./AppWindow/Window";
 import MovableBar from "./AppWindow/Window/MovableBar";
-import BarButtonGroup from "./AppWindow/Window/MovableBar/BarButtonGroup";
-
-import Typography from "@mui/material/Typography";
-
-import { AppIcon } from "./collection/Collection";
 
 import { ReactElement, useEffect } from "react";
 import { focusApp, useAppDispatch } from "../../redux";
