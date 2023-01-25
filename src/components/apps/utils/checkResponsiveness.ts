0ac5e26e05cd15ae30c12dd46a6e7575742c9599
Miouss <samir.ghabi@gmail.com>
2023-01-25 21:00:28 +0100

    Create the responsive handler function

---
+++ src/components/apps/utils/checkResponsiveness.ts
@@ -0,0 +1,22 @@
+export default function checkResponsiveness(
+  currentWidth,
+  dispatch,
+  setWindowResponsiveFont
+) {
+  if (currentWidth < 1720 && currentWidth >= 1540) {
+    dispatch(setWindowResponsiveFont(12));
+    return;
+  }
+
+  if (currentWidth < 1540 && currentWidth >= 1280) {
+    dispatch(setWindowResponsiveFont(8));
+    return;
+  }
+
+  if (currentWidth < 1280 && currentWidth >= 800) {
+    dispatch(setWindowResponsiveFont(6));
+    return;
+  }
+
+  dispatch(setWindowResponsiveFont(16));
+}
