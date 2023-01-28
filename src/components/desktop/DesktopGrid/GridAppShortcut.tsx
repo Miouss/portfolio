81129002aac7491184b159650e8953f6c23cb802
Miouss <samir.ghabi@gmail.com>
2023-01-28 16:25:48 +0100

    fix behavior of shortcuts app

--- src/components/desktop/DesktopGrid/GridAppShortcut.tsx
+++ src/components/desktop/DesktopGrid/GridAppShortcut.tsx
@@ -1,11 +1,16 @@
-import { ShortcutDesktopIcon, getShortcutLink } from "../../apps/AppWindow/Window/Contents/list";
+import {
+  ShortcutDesktopIcon,
+  getShortcutLink,
+} from "../../apps/AppWindow/Window/Contents/list";
 import ShortcutIcon from "@mui/icons-material/Shortcut";
 import {
+  BackgroundColorLayer,
   GridAppBadge,
   GridAppContainer,
   GridAppIcon,
   GridAppLabel,
 } from "../styled/GridApp";
+import { useState } from "react";
 
 interface Props {
   shortcutName: string;
@@ -13,11 +18,21 @@ interface Props {
 
 export default function ShortcutApp({ shortcutName }: Props) {
   const urlRedirect = getShortcutLink(shortcutName);
+  const [click, setClick] = useState(false);
 
+  const handleClick = () => {
+    setClick(true);
+
+    document.onpointerdown = () => {
+      setClick(false);
+      document.onpointerdown = null;
+    };
+  };
+    console.log(click);
   return (
-    <GridAppContainer
-      onDoubleClick={() => window.open(urlRedirect!)}
-    >
+    <GridAppContainer click={click} onDoubleClick={() => window.open(urlRedirect!)} onClick={handleClick}>
+      <BackgroundColorLayer click={click} />
+
       <GridAppIcon style={{ fontSize: "4rem" }}>
         <ShortcutDesktopIcon name={shortcutName} />
       </GridAppIcon>
