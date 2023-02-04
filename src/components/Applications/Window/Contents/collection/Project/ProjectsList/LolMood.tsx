46aae22b9338f4cb76a350bff68bfad2be410e79
Miouss <samir.ghabi@gmail.com>
2023-02-05 00:56:47 +0100

    Add english lang for description

--- src/components/Applications/Window/Contents/collection/Project/ProjectsList/LolMood.tsx
+++ src/components/Applications/Window/Contents/collection/Project/ProjectsList/LolMood.tsx
@@ -6,7 +6,7 @@ import {
   reactSVG,
   phpSVG,
   mysqlSVG,
-  muiSVG
+  muiSVG,
 } from "../../../../../../../assets/icons/icons";
 
 interface Tech {
@@ -16,11 +16,22 @@ interface Tech {
 
 export default function LolMood() {
   const name = "LOL MOOD";
-  const description = `LoL Mood fût mon premier gros projet mettant en pratique mes connaissances en React, PHP, et MySQL.\n
-  Il s'agit d'un site web concernant le jeu vidéo League of Legends. Le site collecte les données des parties classées jouées via l'API Public de l'éditeur Riot Games et les stockent dans une base de donnée privée en les traitent aupréalables pour avoir divers statistiques. \n
-  Les données sont accessibles via une API privée que j'ai développé en PHP et qui est utilisée par le site web pour afficher les données.\n
-  J'ai décidé de faire ce type de site car étant moi-même joueur de League of Legends et utilisant des sites populaires qui font la même chose, j'ai toujours été intrigué de la façon dont ces sites fonctionnaient et j'ai donc décidé de me lancer ce qui m'a permis de balayer une bonne partie de mes connaissances.`;
+  const description = {
+    eng: `LoL Mood was my first big project putting my knowledge of React, PHP, and MySQL into practice.
+     
+    It's a website about the video game League of Legends. The site collects data from ranked games played through Riot Games' public API and stores it in a private database after processing it to have various statistics.
+      
+     The data is accessible through a private API that I developed in PHP and is used by the website to display the data.
+       
+      I decided to make this type of site because as a League of Legends player and user of popular sites that do the same thing, I have always been intrigued by how these sites work, which allowed me to brush up on a good portion of my knowledge.`,
+    fr: `LoL Mood fût mon premier gros projet mettant en pratique mes connaissances en React, PHP, et MySQL.
 
+      Il s'agit d'un site web concernant le jeu vidéo League of Legends. Le site collecte les données des parties classées jouées via l'API Public de l'éditeur Riot Games et les stockent dans une base de donnée privée en les traitent aupréalables pour avoir divers statistiques.
+      
+      Les données sont accessibles via une API privée que j'ai développé en PHP et qui est utilisée par le site web pour afficher les données.
+      
+      J'ai décidé de faire ce type de site car étant moi-même joueur de League of Legends et utilisant des sites populaires qui font la même chose, j'ai toujours été intrigué de la façon dont ces sites fonctionnaient et j'ai donc décidé de me lancer ce qui m'a permis de balayer une bonne partie de mes connaissances.`,
+  };
   const imageUrl = [LolMood1, LolMood2, LolMood3];
 
   const techs: Tech[] = [
@@ -29,8 +40,8 @@ export default function LolMood() {
       icon: reactSVG,
     },
     {
-        name: "MUI",
-        icon: muiSVG,
+      name: "MUI",
+      icon: muiSVG,
     },
     {
       name: "PHP",
--- src/components/Applications/Window/Contents/collection/Project/ProjectsList/Portofolio.tsx
+++ src/components/Applications/Window/Contents/collection/Project/ProjectsList/Portofolio.tsx
@@ -15,15 +15,26 @@ interface Tech {
 
 export default function Portofolio() {
   const name = "Portofolio Windows 10";
-  const description = `Le portofolio étant la vitrine du développeur web autodidacte, je me devais de faire un projet 
-  qui sort de la norme afin de me démarquer. J'ai donc décidé de faire un portofolio ayant 
-  le comportement de Windows 10.
+  const description = {
+    eng: `As a self-taught web developer, the portfolio is the showcase, I had to make a project that stands out from the norm to set myself apart. So I decided to make a portfolio with the behavior of Windows 10.
 
-  L'intégralité du projet a été fait from scratch, sans composants/fonctionnalités pré-fabriqués.
+    The entire project was done from scratch, without pre-fabricated components/features.
+
+    I wanted to stay as close as possible to the OS interface, using the same 
+    fonts, colors, icons, etc. The complexity of managing such an interface, particularly the window system, allowed me to discover Redux, 
+    to better understand the functioning of React, and greatly improve my skills in all aspects of the front-end.`,
+    fr: `Le portofolio étant la vitrine du développeur web autodidacte, je me devais de faire un projet 
+    qui sort de la norme afin de me démarquer. J'ai donc décidé de faire un portofolio ayant 
+    le comportement de Windows 10.
   
-  J'ai voulu rester le plus fidèle possible à l'interface de l'OS, en utilisant les mêmes 
-  polices, les mêmes couleurs, les mêmes icônes, etc. La complexité de gérer une telle interface notamment le systeme de fenêtre m'a permis de découvrir Redux, de mieux comprendre le fonctionnement de React et d'améliorer grandement mes compétences sur tous les aspects du front-end.
-  `;
+    L'intégralité du projet a été fait from scratch, sans composants/fonctionnalités pré-fabriqués.
+    
+    J'ai voulu rester le plus fidèle possible à l'interface de l'OS, en utilisant les mêmes 
+    polices, les mêmes couleurs, les mêmes icônes, etc. La complexité de gérer une telle interface 
+    notamment le systeme de fenêtre m'a permis de découvrir Redux, de mieux comprendre le fonctionnement de React 
+    et d'améliorer grandement mes compétences sur tous les aspects du front-end.
+    `,
+  };
   const imageUrl = [Portofolio1, Portofolio2, Portofolio3];
 
   const techs: Tech[] = [
