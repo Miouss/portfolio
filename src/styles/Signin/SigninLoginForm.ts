a0c8487d8eea7a8d642aadad713d881905761fb1
Miouss <samir.ghabi@gmail.com>
2023-02-21 21:34:05 +0100

    Change fix design for fluid design

--- src/styles/Signin/SigninLoginForm.ts
+++ src/styles/Signin/SigninLoginForm.ts
@@ -3,11 +3,10 @@ import styled from "@mui/system/styled";
 import { Stack } from "@mui/material";
 
 export const LoginFormContainer = styled(Stack)({
-  marginTop: "150px",
+  marginTop: "8%",
   width: "100%",
   maxWidth: "300px",
   "& > *": {
     textAlign: "center",
-    width: "100%",
   },
 });
