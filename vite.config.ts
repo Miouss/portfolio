b3b5af7d5d740934cfe15886121c02136bb7a825
Miouss <samir.ghabi@gmail.com>
2023-02-21 23:00:52 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteRequire } from 'vite-require'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteRequire } from "vite-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +9,4 @@ export default defineConfig({
     open: true,
     port: 3000,
   },
-})
+});
