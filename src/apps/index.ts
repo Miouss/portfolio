2933b0182ccb55161394258770f673ec47c49d17
Miouss <samir.ghabi@gmail.com>
2023-10-14 15:49:34 +0200

    update projects' folder

--- src/apps/index.ts
+++ src/apps/index.ts
@@ -1,5 +1,5 @@
 export { default as TerminalApp } from "./Terminal/Terminal";
-export { default as ProjectsViewer } from "./collections/Projects";
+export { default as ProjectsViewer } from "./Projects/Projects";
 export { default as MailSender } from "./MailSender/MailSender";
 export { default as ChillBeats } from "./ChillBeats/ChillBeats";
 export { default as Notepad } from "./Notepad/Notepad";
